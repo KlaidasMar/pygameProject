c808ce763a3e6e7e111cc67f2812707c12f5d2af
Klaidas Markisius <markisius.klaidas@gmail.com>
2023-03-20 15:54:29 +0200

    -

--- main.py
+++ main.py
@@ -11,11 +11,21 @@ WIDTH = 500
 screen = pygame.display.set_mode((HEIGHT, WIDTH))
 pygame.display.set_caption("Space shooter")
 
+SPACESHIP_HEIGHT = 55
+SPACESHIP_WIDTH = 40
+
 BG = pygame.image.load('Assets/space.png')
-WHITE = (255, 255, 255)
+RED_SPACESHIP_IMG = pygame.image.load('Assets/spaceship_red.png')
+RED_SPACESHIP = pygame.transform.scale(RED_SPACESHIP_IMG, (SPACESHIP_HEIGHT, SPACESHIP_WIDTH))
+YELLOW_SPACESHIP_IMG = pygame.image.load('Assets/spaceship_yellow.png')
+YELLOW_SPACESHIP = pygame.transform.scale(YELLOW_SPACESHIP_IMG, (SPACESHIP_HEIGHT, SPACESHIP_WIDTH))
+
 
 def draw_screen():
-    screen.fill(WHITE)
+
+    screen.blit(BG, (0, 0))
+    screen.blit(RED_SPACESHIP, (100, 250))
+    screen.blit(YELLOW_SPACESHIP, (700, 250))
     pygame.display.update()
 
 
